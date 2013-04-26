339b2a9533f6265ab54b368c46759f6cf6235705
john <john@hesse>
2013-04-26 15:53:56 +0200

    move scripts to scripts directory

---
+++ scripts/install_db-prod.sh
@@ -0,0 +1,7 @@
+#usage: ./install_db-prod.sh hostname port_nr
+
+PSQL="psql -h $1 -p $2"
+
+#must have user linkdump and db linkdump on host
+
+$PSQL -U linkdump linkdump_prod -f database/schema.sql

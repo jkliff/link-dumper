9385a74103a8b98e785048cd10df0db0576425eb
john <john@hesse>
2013-04-27 16:07:20 +0200

    change name to link_dumper

--- scripts/install_db-prod.sh
+++ scripts/install_db-prod.sh
@@ -4,4 +4,4 @@ PSQL="psql -h $1 -p $2"
 
 #must have user linkdump and db linkdump on host
 
-$PSQL -U linkdump linkdump_prod -f database/schema.sql
+$PSQL -U linkdump linkdump_prod -f database/20_api.sql

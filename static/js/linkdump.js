010824252f8c557e4d18648928e0618215b3b884
john <john@hesse>
2013-05-09 13:07:13 +0200

    selection of identified links

--- static/js/linkdump.js
+++ static/js/linkdump.js
@@ -36,51 +36,96 @@ $('#bulkImportSubmit').click (function () {
 
     var d = $('#formBulkImport');
 
-    var resolvedUrls = $('#resolvedUrlList');
+    var resolvedUrls = $('#resolvedUrlsTableBody');
     resolvedUrls.empty();
+
     $('#bulkImportSubmitProgress').removeClass ('hidden');
     $('#bulkImportSubmit').addClass ('hidden');
 
     var p = {
-        q: d.ql && d.ql != null? d.ql : d.qu,
+        q: d.find ('textarea[name=qu]').hasClass ('hidden') ? d.find ('input[name=ql]').val() : d.find ('textarea[name=qu]').val(),
         mode: 'check'
     };
 
     $.ajax ({
         type: 'POST',
         url: 'perform_bulk_import',
-        data: $(p).serialize ()
+        data: p
     }).complete (function (data) {
+
+        console.log ('received', data);
 
         $('#bulkImportSubmitProgress').addClass ('hidden');
         $('#bulkImportSubmit').removeClass ('hidden');
 
         var r = $.parseJSON (data.responseText);
 
-        if (!r.urls) return;
+        if (!r.urls || r.urls.length < 1) {
+            return;
+        }
 
         // fixme: read data to be sent from variable
-        var q = $ ('#formConfirmBulkImport [name=q]');
+        var q = $ ('#formConfirmBulkImport[name=q]');
 
         q.val (r.urls.join (' '));
 
         $ ('#resolvedUrls').removeClass ('hidden');
 
+        var tb = resolvedUrls ;// $('#resolvedUrlsTableBody');
+        if (!tb) {
+            console.log ('no table');
+            return;
+        }
+
         $.each (r.urls, function (idx, val) {
             var existing = r.existing.indexOf (val) > -1;
-            var hint = existing ? '<span class="label label-important">duplicated</span> ' : '';
+            var hint = existing ? '<span class="duplicateHint" class="label label-important">duplicated</span>' : '<span class="label" id="label">Include</span>';
 ;
             console.log ('existing? ', hint, val);
-            resolvedUrls.append ('<li class="">' + hint + val + '</li>');
+
+            var tr = $('<tr></tr>'); //+ hint + val + '</span></li>');
+            var td = $('<td></td>');
+            td.append (hint)
+
+            if (!existing) {
+                td.click (LinkSelectionToggleFactory.create (val, td));
+            }
+
+            tr.append (td);
+            td = $('<td><pre></pre></td>');
+            td.text(val);
+            tr.append (td);
+            console.log (tr);
+                        tb.append (tr);
+            console.log (tb);
         });
+        console.log (tb);
 
         if (r.urls.length > 0) {
-            $('#formConfirmBulkImport .btn').removeAttr("disabled").addClass ("btn-primary");
+            $('#formConfirmBulkImportConfirm').removeAttr("disabled").addClass ("btn-primary");
         }
 
     }).error (DEFAULT_ERROR_HANDLER);
 
 });
+
+
+LinkSelectionToggleFactory = {
+
+    create: function (url, el) {
+        return function (data) {
+            console.log ('selected', url);
+            var s = el.find ('span');
+            if (s.hasClass ('label-success')) {
+                s.removeClass ('label-success');
+                s.text ('Include');
+            } else {
+                s.addClass ('label-success');
+                s.text ('Included');
+            }
+        }
+    }
+};
 
 $('#formMainSearch').submit (function () {
     var q = $('#formMainSearch').serialize();

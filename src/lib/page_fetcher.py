8f8bc3b2854e7a9181ad91bdd380a136ad3d2d90
john <john@hesse>
2013-06-22 15:02:32 +0200

    format

--- src/lib/page_fetcher.py
+++ src/lib/page_fetcher.py
@@ -1,4 +1,3 @@
-import os
 import re
 
 import urllib2
@@ -6,24 +5,26 @@ from urllib2 import URLError
 from urllib2 import HTTPError
 
 
-def save_file (page_title, data):
+def save_file(page_title, data):
     """
     >>> re.sub ('[^-a-zA-Z0-9_. ]+', '',\
         re.sub ('[/(): ]+', '_',\
         re.sub ('\.html?$', '', 'http://iua.de/hrf-/,3420%=af?re&a\df_&aer=iuah/rsi:f')))
 
 """
-    file_name = re.sub ('[^-a-zA-Z0-9_. ]+', '',
-        re.sub ('[/(): ]+', '_',
-        re.sub ('\.html?$', '', page_title)
-    ))
-    with (open (file_name, 'w')) as f:
-        f.write (data)
+    file_name = re.sub('[^-a-zA-Z0-9_. ]+', '',
+                       re.sub('[/(): ]+', '_',
+                              re.sub('\.html?$', '', page_title)
+                       ))
+    with (open(file_name, 'w')) as f:
+        f.write(data)
 
-def parse_title (data):
+
+def parse_title(data):
     return None
 
-def fetch (url):
+
+def fetch(url):
     """
     >>> fetch ('http://localhost:8000/test_page.html')
     >>> fetch ('http://localhost:8000/not_here.html')
@@ -33,15 +34,15 @@ def fetch (url):
 """
 
     try:
-        resp = urllib2.urlopen (url)
+        resp = urllib2.urlopen(url)
         data = resp.read()
-        title = parse_title (data)
+        title = parse_title(data)
         if title is None:
             title = 'UNK ' + url
-        save_file (title, data)
+        save_file(title, data)
     except URLError as e:
         print e
-    except HTTPError as e :
+    except HTTPError as e:
         print e
 
 
